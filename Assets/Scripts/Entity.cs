1720c63d69b8b4bf530c6b253f53e98ecd5a3568
Di <13022698969@163.com>
2026-04-06 15:09:51 +0800

    增加击退效果

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -11,6 +11,12 @@ public class Entity : MonoBehaviour
     public EntityFX fx { get;private set; }
     
     #endregion
+
+    [Header("knockback info")] 
+    [SerializeField] protected Vector2 knockbackDirection;
+    [SerializeField] protected float knockbackDuration;
+    protected bool isknocked;
+    
     [Header("Collision info")] 
     public Transform attackCheck;
     public float attackCheckRadius;
@@ -42,15 +48,36 @@ public class Entity : MonoBehaviour
     public virtual void Damage()
     {
         fx.StartCoroutine("flashFX");
+        StartCoroutine("HitKnockback");
+        
         Debug.Log(gameObject.name + " damage");
     }
 
+    protected virtual IEnumerator HitKnockback()
+    {
+        isknocked = true;
+
+        rb.velocity = new Vector2(knockbackDirection.x * -facingDir, knockbackDirection.y);
+        
+        yield return new WaitForSeconds(knockbackDuration);
+        isknocked = false;
+    }
+
     #region Velocity
-    public void SetZeroVelocity() => rb.velocity = new Vector2(0, 0);
+
+    public void SetZeroVelocity()
+    {
+        if (isknocked)
+            return;
+        rb.velocity = new Vector2(0, 0);
+    }
 
 
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
+        if (isknocked)
+            return;
+        
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
         FlipController(_xVelocity);
     }
