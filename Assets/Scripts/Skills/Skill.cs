c92b2c5c73e82a842c8b61547e7a68e1766fc7f2
Di <13022698969@163.com>
2026-04-10 22:48:42 +0800

    创建剑prefab并设计idle与flip动画及相关状态，按下右键可以掷出剑

--- Assets/Scripts/Skills/Skill.cs
+++ Assets/Scripts/Skills/Skill.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,13 @@ public class Skill : MonoBehaviour
 {
     [SerializeField] protected float cooldown;
     protected float cooldownTimer;
+
+    protected Player player;
+
+    protected virtual void Start()
+    {
+        player = PlayerManager.instance.player;
+    }
 
     protected virtual void Update()
     {
