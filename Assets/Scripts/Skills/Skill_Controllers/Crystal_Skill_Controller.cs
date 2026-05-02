8e28114ba152470328e9601f3042878d5b1b866e
Di <13022698969@163.com>
2026-05-02 16:13:52 +0800

    初始化水晶

---
+++ Assets/Scripts/Skills/Skill_Controllers/Crystal_Skill_Controller.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal_Skill_Controller : MonoBehaviour
+{
+    private float crystalExistTimer;
+    
+    public void SetupCrystal(float _crystalDuration)
+    {
+        crystalExistTimer = _crystalDuration;
+    }
+
+    private void Update()
+    {
+        crystalExistTimer -= Time.deltaTime;
+        
+        if (crystalExistTimer < 0)
+        {
+            SelfDestroy();
+        }
+    }
+    
+    public void SelfDestroy() => Destroy(gameObject);
+}
