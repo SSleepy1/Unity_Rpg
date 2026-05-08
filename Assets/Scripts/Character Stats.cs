d85e16ad0e92b06ff8fae40cd39e4f2030724a5b
Di <13022698969@163.com>
2026-05-08 19:30:45 +0800

    新增力量，敏捷，魔力，生命属性并增加对应伤害结算

--- Assets/Scripts/Character Stats.cs
+++ Assets/Scripts/Character Stats.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat strength;
-    public Stat damage;
+    [Header("Major stats")]
+    public Stat strength;//每一点力量提升百分之一攻击力
+    public Stat agility;//每一点敏捷可以提升百分之一闪避
+    public Stat intelligence;//每一点魔力可以提升百分之一法强和3魔抗
+    public Stat vitality;//每一点生命可以提升3生命值
+    
+    [Header("Defensive stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+    
+    
+    
+    public Stat damage;
 
     [SerializeField]private int currentHealth;
     protected virtual void Start()
@@ -16,11 +29,15 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
-        
+        if (TargetCanAvoidAttack(_targetStats))
+            return;
+
         int totalDamage = damage.GetValue() + strength.GetValue();
-        
+
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
         _targetStats.TakeDamage(totalDamage);
     }
+
 
     public virtual void TakeDamage(int _damage)
     {
@@ -37,4 +54,24 @@ public class CharacterStats : MonoBehaviour
     {
         
     }
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+
+        //将伤害限制在0~max之间，防止出现负数回血的情况
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+
 }
