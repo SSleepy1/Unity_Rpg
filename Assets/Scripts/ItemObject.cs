ab154302b1ee35e8554e96de957ad3f2483b129a
Di <13022698969@163.com>
2026-05-14 17:41:30 +0800

    新增物品资源

---
+++ Assets/Scripts/ItemObject.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    
+    [SerializeField] private ItemData itemData;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        sr.sprite = itemData.Icon;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            Debug.Log("Picked Up item" + itemData.itemName);
+            Destroy(gameObject);
+        }
+    }
+}
